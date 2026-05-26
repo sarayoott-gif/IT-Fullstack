8f5c9e9e4cc422125c580f8268c0033b5c242877
sarayoott-gif <sarayoot.t@itm.kmutnb.ac.th>
2026-05-26 11:00:53 +0700

    Add Books

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello Anirach Good World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
